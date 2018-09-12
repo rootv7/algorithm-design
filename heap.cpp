0ec370425ef7eb157ebf30bc36a550b7febe2a79
ritbit1111 <ritesh.play1@gmail.com>
2018-09-12 22:15:12 +0530

    heap implementation array

--- heap.cpp
+++ heap.cpp
@@ -2,69 +2,75 @@
 
 using namespace std;
 
-struct node{
-	int element;
-	node* parent;
-	node* leftchild;
-	node* rightchild;
-};
-
 class heap{
-	node* root;
-	node* position;
+	int btree[1000];
+	int index;
 public:
-	heap(node* root){
-		this.root = root;
+	heap(int root){
+		btree[0] = root;
+		index = 1;
+	}
+	int findmin(){
+		return btree[0];
 	}
 	void insert(int element);
-	void bubble_up(node* position);
-	void update_position();
-	node* leftmostchild(node* root){
-		if(root->leftchild){
-			leftmostchild(root->leftchild)
-		}
-		else{
-			return root;
-		}
+	void delmin();
+	void show(){
+		for(int i = 0; i < index; i++)
+			cout<<btree[i]<<endl;
 	}
-	/*void delmin() {
-		delmin(this.root);
-	}
-	int findmin();
-	void delmin(node* root);
-	~heap(){
-		if(root->rightchild)
-	}*/
 };
+
 void heap::insert(int element){
-	position->element = element;
-	bubble_up(position);
-}
-void heap::bubble_up(node* position){
-	if(position->parent){
-		if(position->parent->element > position->element){
-			int element = position->element;
-			position->element = position->parent->element
-			position->parent->element = element;
-			insert(position->parent)
-		}
+	btree[index] = element;
+	int pos = index, parent_pos;
+	while(1){
+		int parent_pos = (pos - 1)/2;
+		if(pos == 0 || btree[parent_pos] <= btree[pos])
+			break;
+		btree[pos] = btree[parent_pos];
+		btree[parent_pos] = element;
+		pos = parent_pos;
 	}
+	index++;
 }
-void heap::update_position(){
-	if(position->parent){
-		if(position->parent->leftchild == position){
-			position = parent->rightchild->leftmostchild();
-			node* new_position = new node*;
-			position->leftchild = new_position;
-			new_position->parent = position;
-			position = new_position;
+
+void heap::delmin(){
+	btree[0] = btree[index - 1];
+	index--;
+	int pos = 0;
+	while(1){
+		int right_pos = pos*2 + 2;
+		int left_pos = pos*2 + 1;
+		int swap_index;
+		if(left_pos >= index){
+			break;
+		}else if(right_pos == index){
+			swap_index = left_pos;
+		}else{
+			if(btree[right_pos] > btree[left_pos])
+				swap_index = left_pos;
+			else
+				swap_index = right_pos;
 		}
-		else{
-			postion = parent->position
-		}
+		if(btree[swap_index] >= btree[pos])
+			break;
+		int temp = btree[swap_index];
+		btree[swap_index] = btree[pos];
+		btree[pos] = temp;
+		pos = swap_index;
 	}
 }
 
 int main(){
-
+	heap H(23);
+	H.insert(5);
+	H.insert(19);
+	H.insert(2);
+	H.insert(21);
+	H.insert(32);
+	H.show();
+	cout<<H.findmin()<<endl;
+	H.delmin();
+	H.show();
 }
